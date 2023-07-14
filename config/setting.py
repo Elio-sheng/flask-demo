9e5a2db890bd3ac4a4065c6f085e7d4bb4cb6c8c
Sheng <elio.sheng@easyhealthcarecorp.com>
2023-07-14 17:39:23 +0800

    init

---
+++ config/setting.py
@@ -0,0 +1,19 @@
+# 服务端口配置
+SERVER_PORT = 9999
+
+# MySQL配置
+MYSQL_HOST = "10.0.9.52"
+MYSQL_PORT = 3306
+MYSQL_USER = "root"
+MYSQL_PASSWD = "premom123456"
+MYSQL_DB = "flask_demo"
+
+# Redis配置
+REDIS_HOST = "10.0.9.61"
+REDIS_PORT = 6379
+REDIS_PASSWD = ""
+# token过期时间(单位：秒)
+EXPIRE_TIME = 600
+
+# MD5加密盐值
+MD5_SALT = "test202#%*"
